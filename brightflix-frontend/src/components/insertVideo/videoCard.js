3780950f0277d888bfbb6d4bfa1daaa36b963553
Simao <simneves@deloitte.pt>
2024-06-28 16:31:03 +0100

    att

--- brightflix-frontend/src/components/insertVideo/videoCard.js
+++ brightflix-frontend/src/components/insertVideo/videoCard.js
@@ -52,7 +52,7 @@ function VideoCard({ video }) {
         <div className='trailerPopup'>
           <div className='trailerContent'>
             <iframe 
-              src={`${video.video}?autoplay=1`} 
+              src={`${video.video}?autoplay=1&mute=1`} 
               title="YouTube video player" 
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
               allowFullScreen>
